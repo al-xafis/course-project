2c704569716362c2f0fad01c7e058a94933c79fb
Al-Khafis Muzrafkulov <alkh4fis@gmail.com>
2024-05-29 06:26:22 +0500

    fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,23 +3,40 @@
 namespace App\DataFixtures;
 
 use App\Entity\CollectionCategory;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(private UserPasswordHasherInterface $passwordHasher) {
+    }
+
     public function load(ObjectManager $manager): void
     {
         $books = new CollectionCategory();
-        $books->setName('books');
+        $books->setName('Books');
         $coins = new CollectionCategory();
-        $coins->setName('coins');
+        $coins->setName('Coins');
         $arts = new CollectionCategory();
-        $arts->setName('arts');
+        $arts->setName('Arts');
 
         $manager->persist($books);
         $manager->persist($coins);
         $manager->persist($arts);
+
+        $admin = new User();
+        $admin->setEmail('admin@mail.ru');
+        $admin->setStatus('Active');
+        $admin->setFirstName('Admin');
+        $admin->setLastName('Admin');
+        $admin->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+        $password = 1;
+        $hashedPassword = $this->passwordHasher->hashPassword($admin, $password);
+        $admin->setPassword($hashedPassword);
+
+        $manager->persist($admin);
 
         $manager->flush();
     }
